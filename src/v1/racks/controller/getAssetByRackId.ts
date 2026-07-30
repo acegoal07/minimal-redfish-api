3927939663966f69387c837854d2ffdd728f6357
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-07-30 10:09:04 +0100

    Update getAssetByRackId.ts

--- src/v1/racks/controller/getAssetByRackId.ts
+++ src/v1/racks/controller/getAssetByRackId.ts
@@ -2,7 +2,11 @@ import { Hono } from 'hono';
 import { prisma } from '../../../lib/prisma';
 import { zValidator } from '@hono/zod-validator';
 import { z } from 'zod';
-import { internalServerError, invalidParametersError } from '../../../lib/errorMessages';
+import {
+   internalServerError,
+   invalidParametersError,
+   notFoundError
+} from '../../../lib/errorMessages';
 import { getValueFromJson } from '../../../lib/util';
 
 export default new Hono().get(
@@ -25,73 +29,60 @@ export default new Hono().get(
       try {
          const { id } = c.req.valid('param');
 
-         // Get all the assets using the id of the rack they are linked to
-         const assets = await prisma.asset.findMany({
+         // Try and get the rack from the database
+         const rack = await prisma.rack.findUnique({
             where: {
-               rackId: id
+               id
+            },
+            include: {
+               assets: {
+                  include: {
+                     jsonHistory: {
+                        orderBy: {
+                           uploadDate: 'desc'
+                        },
+                        take: 1
+                     },
+                     paths: true,
+                     _count: {
+                        select: {
+                           jsonHistory: true
+                        }
+                     }
+                  }
+               }
             }
          });
 
-         // Go through each asset and gather the asset data
-         const assetsWithJson = await Promise.all(
-            assets.map(async (asset) => {
-               // Get all the data fields from the database
-               const assetPaths = await prisma.path.findMany({
-                  where: {
-                     assetId: asset.id
-                  }
-               });
+         // Check if the rack exists
+         if (!rack) {
+            return notFoundError(c);
+         }
 
-               // Get the total number of json history items in the database for the asset
-               const totalJsonHistory = await prisma.jsonHistory.count({
-                  where: {
-                     assetId: asset.id
-                  }
-               });
-
-               // Get the latest json history for the asset
-               const latestJsonHistory = await prisma.jsonHistory.findFirst({
-                  where: {
-                     assetId: asset.id
-                  },
-                  orderBy: {
-                     uploadDate: 'desc'
-                  }
-               });
-
-               // Get the values for the asset paths from the json
-               const assetPathsWithData = assetPaths.map((path) => {
-                  return {
-                     ...path,
-                     value: getValueFromJson<string>(latestJsonHistory, path.path)
-                  };
-               });
-
-               return {
-                  id: asset.id,
-                  name: asset.name,
-                  position: asset.position,
-                  size: asset.size,
-                  rackId: asset.rackId,
-                  data: assetPathsWithData.map((path) => ({
-                     id: path.id,
-                     name: path.name,
-                     path: path.path,
-                     value: path.value
-                  })),
-                  json: {
-                     text: latestJsonHistory?.rawJson,
-                     filename: latestJsonHistory?.filename
-                  },
-                  pagination: {
-                     position: 0,
-                     total: totalJsonHistory
-                  }
-               };
-            })
+         return c.json(
+            rack.assets.map((asset) => ({
+               id: asset.id,
+               name: asset.name,
+               position: asset.position,
+               size: asset.size,
+               rackId: asset.rackId,
+               data: asset.paths.map((path) => ({
+                  id: path.id,
+                  name: path.name,
+                  path: path.path,
+                  value: getValueFromJson<string>(asset.jsonHistory[0].rawJson, path.path)
+               })),
+               json: {
+                  text: asset.jsonHistory[0]?.rawJson,
+                  filename: asset.jsonHistory[0]?.filename
+               },
+               pagination: {
+                  position: 0,
+                  total: asset._count.jsonHistory
+               }
+            })),
+            200
          );
-
-         return c.json(assetsWithJson, 200);
       } catch (err) {
          return internalServerError(c, err);
       }
