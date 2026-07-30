c70ba2ddac440391f0325dfe19e2af8f67317e08
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-07-30 13:07:43 +0100

    trailing

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import 'dotenv/config';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { serve } from '@hono/node-server';
+import { trimTrailingSlash } from 'hono/trailing-slash';
 import v1 from './v1';
 
 const app = new Hono();
 
 app.use('*', cors());
+app.use('*', trimTrailingSlash());
 
 app.route('/api/v1', v1);
 
