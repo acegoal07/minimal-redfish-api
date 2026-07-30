fae930d5764bef8156a1d2b2bf6421b7766150d8
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-07-30 10:44:34 +0100

    cors fix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import { serve } from '@hono/node-server';
 import v1 from './v1';
 
 const app = new Hono();
+
+app.use('*', cors());
 
 app.route('/api/v1', v1);
 
