d31dd9bbef67f3add7c613947f8c8b5b2b4ea6b5
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-08-06 14:23:17 +0100

    Update init.sql

--- init.sql
+++ init.sql
@@ -26,7 +26,7 @@ CREATE TABLE
    );
 
 CREATE TABLE
-   _RoleToPermission (
+   _RolePermission (
       A INT NOT NULL,
       B INT NOT NULL,
       PRIMARY KEY (A, B),
@@ -219,7 +219,7 @@ VALUES
 -- ===========================================================
 -- Give admin every permission
 -- ===========================================================
-INSERT IGNORE INTO _RoleToPermission (A, B)
+INSERT IGNORE INTO _RolePermission (A, B)
 SELECT
    r.id,
    p.id
