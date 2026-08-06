95fe03284ecf369384d76af9dac292f39606f72a
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-08-06 11:35:22 +0100

    get current user

--- src/v2/users/index.ts
+++ src/v2/users/index.ts
@@ -6,11 +6,13 @@ import deleteUser from './controllers/deleteUser';
 import refreshLogin from './controllers/refreshLogin';
 import logoutUser from './controllers/logoutUser';
 import updateUser from './controllers/updateUser';
+import getCurrentUser from './controllers/getCurrentUser';
 
 export default new Hono()
    .route('/login', loginUser)
    .route('/refresh', refreshLogin)
    .route('/logout', logoutUser)
+   .route('/current', getCurrentUser)
    .route('/:id', updateUser)
    .route('/', createUser)
    .route('/', deleteUser);
