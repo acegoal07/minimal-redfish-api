853aa78988624538fd5d2c0b35968183d3f853cb
Oscar <oscmog@gmail.com>
2026-08-07 10:35:30 +0100

    Assigned route to deleting a path

--- src/v2/assets/index.ts
+++ src/v2/assets/index.ts
@@ -4,8 +4,10 @@ import addPathToAsset from './controllers/paths/addPathToAsset';
 import createServerAsset from './controllers/servers/createServerAsset';
 import createStorageAsset from './controllers/storages/createStorageAsset';
 import getAllAssets from './controllers/getAllAssets';
+import deleteAssetPath from './controllers/paths/deleteAssetPath';
 
 export default new Hono()
+   .route('/:id/paths/:pathId', deleteAssetPath)
    .route('/:id/paths', addPathToAsset)
    .route('/:id', deleteAsset)
    .route('/servers', createServerAsset)
