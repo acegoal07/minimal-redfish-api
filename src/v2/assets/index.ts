86dff9eef2dd69e28d67baf30981ec02d5479806
Oscar <oscmog@gmail.com>
2026-08-07 13:31:16 +0100

    Registersd asset history

--- src/v2/assets/index.ts
+++ src/v2/assets/index.ts
@@ -7,12 +7,14 @@ import getAllAssets from './controllers/getAllAssets';
 import deleteAssetPath from './controllers/paths/deleteAssetPath';
 import getAssetPaths from './controllers/paths/getAssetPaths';
 import updateAssetPath from './controllers/paths/updateAssetPath';
+import getAssetHistory from './controllers/history/getAssetHistory';
 
 export default new Hono()
    .route('/:id/paths/:pathId', deleteAssetPath)
    .route('/:id/paths', addPathToAsset)
    .route('/:id/paths', getAssetPaths)
    .route('/:id/paths', updateAssetPath)
+   .route('/:id/history', getAssetHistory)
    .route('/:id', deleteAsset)
    .route('/servers', createServerAsset)
    .route('/storages', createStorageAsset)
