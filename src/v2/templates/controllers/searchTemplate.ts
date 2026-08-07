7b38a9a302e5484e98377ffd10b3a66b01665397
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-08-07 13:20:02 +0100

    tweak

--- src/v2/templates/controllers/searchTemplate.ts
+++ src/v2/templates/controllers/searchTemplate.ts
@@ -1,65 +1,67 @@
 import { Hono } from 'hono';
-import { z } from 'zod';
 
 import { prisma } from '../../../lib/prisma';
 import { forbiddenError, internalServerError } from '../../../lib/errorMessages';
 import { validatePermissions } from '../../../lib/util';
-import { queryValidator } from '../../../lib/validators';
+import { searchQueryValidator } from '../../../lib/validators';
 
 export default new Hono().get(
    '/',
-   queryValidator(
-      z
-         .object({
-            query: z.string().trim().optional()
-         })
-         .transform((value) => ({
-            query: value.query,
-            id: value.query ? Number(value.query) : undefined
-         }))
-   ),
+   searchQueryValidator({}),
    async (c) => {
-      // Check users permissions
-      if (!validatePermissions(['template.read'], c)) {
-         return forbiddenError(c);
-      }
-
-      // Get request information
-      const { query, id } = c.req.valid('query');
-
-      // Returns blank if there is no query
-      if (!query) {
-         return c.json([], 200);
-      }
-
-      // Search for templates
       try {
-         const templates = await prisma.template.findMany({
-            where: {
-               OR: [
-                  ...(Number.isInteger(id) ? [{ id }] : []),
-                  {
-                     name: {
-                        contains: query
+         // Check users permissions
+         if (!validatePermissions(['template.read'], c)) {
+            return forbiddenError(c);
+         }
+
+         // Get request information
+         const { query, page, limit } = c.req.valid('query');
+
+         // Returns blank if there is no query
+         if (!query) {
+            return c.json([], 200);
+         }
+
+         // Search for templates
+         const [templates, total] = await prisma.$transaction([
+            prisma.template.findMany({
+               where: {
+                  OR: [
+                     ...(Number.isInteger(Number(query)) ? [{ id: Number(query) }] : []),
+                     {
+                        name: {
+                           contains: query
+                        }
                      }
-                  }
-               ]
-            },
-            include: {
-               paths: true
-            }
-         });
+                  ]
+               },
+               skip: (page - 1) * limit,
+               take: limit,
+               include: {
+                  paths: true
+               }
+            }),
+
+            prisma.template.count()
+         ]);
 
          return c.json(
-            templates.map((template) => ({
-               id: template.id,
-               name: template.name,
-               paths: template.paths.map((path) => ({
-                  id: path.id,
-                  name: path.name,
-                  path: path.name
-               }))
-            })),
+            {
+               templates: templates.map((template) => ({
+                  id: template.id,
+                  name: template.name,
+                  paths: template.paths.map((path) => ({
+                     id: path.id,
+                     name: path.name,
+                     path: path.path
+                  }))
+               })),
+               page,
+               limit,
+               total,
+               totalPage: Math.ceil(total / limit)
+            },
             200
          );
       } catch (err) {
