5989d3cbadbca119669c7e29506b8812a79037e9
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-08-07 13:27:51 +0100

    Improved search

--- src/v2/groups/index.ts
+++ src/v2/groups/index.ts
@@ -5,8 +5,10 @@ import createGroup from './controllers/createGroup';
 import updateGroup from './controllers/updateGroup';
 import deleteGroup from './controllers/deleteGroup';
 import getGroupById from './controllers/getGroupById';
+import searchGroups from './controllers/searchGroups';
 
 export default new Hono()
+   .route('/search', searchGroups)
    .route('/:id', getGroupById)
    .route('/:id', deleteGroup)
    .route('/:id', updateGroup)
--- src/v2/roles/controllers/searchRole.ts
+++ src/v2/roles/controllers/searchRole.ts
@@ -3,92 +3,67 @@ import { z } from 'zod';
 
 import { prisma } from '../../../lib/prisma';
 import { internalServerError } from '../../../lib/errorMessages';
-import { queryValidator } from '../../../lib/validators';
+import { searchQueryValidator } from '../../../lib/validators';
 
-export default new Hono().get(
-   '/',
-   queryValidator(
-      z
-         .object({
-            query: z.string({ error: 'Query must be a string' }).trim().optional(),
-            page: z.coerce
-               .number({ error: 'Page must be a number' })
-               .int({ error: 'Page must be an integer' })
-               .positive({ error: 'Page must be 1 or greater' })
-               .default(1),
-            limit: z.coerce
-               .number({ error: 'Limit must be a number' })
-               .int({ error: 'Limit must be an integer' })
-               .positive({ error: 'Limit must be greater than 0' })
-               .default(25)
-         })
-         .transform((value) => ({
-            query: value.query,
-            id: value.query ? Number(value.query) : undefined,
-            page: value.page,
-            limit: value.limit
-         }))
-   ),
-   async (c) => {
-      try {
-         // Get request information
-         const { query, id, page, limit } = c.req.valid('query');
+export default new Hono().get('/', searchQueryValidator({}), async (c) => {
+   try {
+      // Get request information
+      const { query, page, limit } = c.req.valid('query');
 
-         // Returns blank if there is no query
-         if (!query) {
-            return c.json([], 200);
-         }
-
-         // Search for roles
-         const [roles, total] = await prisma.$transaction([
-            prisma.role.findMany({
-               where: {
-                  OR: [
-                     ...(Number.isInteger(id) ? [{ id }] : []),
-                     {
-                        name: {
-                           contains: query
-                        }
-                     }
-                  ]
-               },
-               include: {
-                  permissions: true
-               },
-               skip: (page - 1) * limit,
-               take: limit
-            }),
-
-            prisma.role.count({
-               where: {
-                  OR: [
-                     ...(Number.isInteger(id) ? [{ id }] : []),
-                     {
-                        name: {
-                           contains: query
-                        }
-                     }
-                  ]
-               }
-            })
-         ]);
-
-         return c.json(
-            {
-               page,
-               limit,
-               total,
-               totalPage: Math.ceil(total / limit),
-               roles: roles.map((role) => ({
-                  id: role.id,
-                  name: role.name,
-                  permissions: role.permissions.map((name) => name)
-               }))
-            },
-            200
-         );
-      } catch (err) {
-         return internalServerError(c, err);
+      // Returns blank if there is no query
+      if (!query) {
+         return c.json([], 200);
       }
+
+      // Search for roles
+      const [roles, total] = await prisma.$transaction([
+         prisma.role.findMany({
+            where: {
+               OR: [
+                  ...(Number.isInteger(Number(query)) ? [{ id: Number(query) }] : []),
+                  {
+                     name: {
+                        contains: query
+                     }
+                  }
+               ]
+            },
+            include: {
+               permissions: true
+            },
+            skip: (page - 1) * limit,
+            take: limit
+         }),
+
+         prisma.role.count({
+            where: {
+               OR: [
+                  ...(Number.isInteger(Number(query)) ? [{ id: Number(query) }] : []),
+                  {
+                     name: {
+                        contains: query
+                     }
+                  }
+               ]
+            }
+         })
+      ]);
+
+      return c.json(
+         {
+            page,
+            limit,
+            total,
+            totalPage: Math.ceil(total / limit),
+            roles: roles.map((role) => ({
+               id: role.id,
+               name: role.name,
+               permissions: role.permissions.map((name) => name)
+            }))
+         },
+         200
+      );
+   } catch (err) {
+      return internalServerError(c, err);
    }
-);
+});
